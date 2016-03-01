c0f98e7e8bb5f358186d522151805ebcaeccd88d
Aaqib Parvez <aaqib29292@gmail.com>
2016-03-02 00:15:09 +0530

    Add User model using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
 
+  devise_for :users
   resources :tasks, except: [:show]
 
   # The priority is based upon order of creation: first created -> highest priority.
