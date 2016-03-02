fcf75b10c05607bb64d0c9ad599c02d5db32c584
Aaqib Parvez <aaqib29292@gmail.com>
2016-03-03 01:08:19 +0530

    little modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  
+
 
   devise_for :users
   resources :tasks, except: [:show]
 
+  resources :task_share, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
